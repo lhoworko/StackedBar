e3053f633ecd84232f46b3d894f4b308b00ec6c9
Lee Howorko <lhoworko@gmail.com>
2015-04-12 11:22:57 -0600

    Add practice round for every new task and system

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var participant_number = 0;
 var task_type = 0;
 var system_type;
 
-var round = 0;
+var round = -1;
 var total_count = 0;
 
 var num_systems = 3;
 var num_tasks = 2;
-var rounds_per_task = 1;
+var rounds_per_task = 3;
 
 var timer;
 var chart;
@@ -62,11 +62,24 @@ function showInstructions() {
 }
 
 function showQuestion() {
+    console.log('system ' + system_type);
+    console.log('task ' + task_type);
+    console.log('round ' + round);
+    console.log('total ' + total_count);
+
     timer.startTimer();
 
     // Create and show the chart.
     $('div#chart_display svg').show();
-    chart.updateChart(system_type, task_type, total_count);
+
+    if (round == -1) {
+        // Practice round
+        chart.updateChart(system_type, task_type,
+                ((num_systems * num_tasks * rounds_per_task) +
+                ((2 * system_type) + task_type)));
+    } else {
+        chart.updateChart(system_type, task_type, total_count);
+    }
 
     $('button#continue_btn').hide();
 }
@@ -95,17 +108,24 @@ function answerQuestion(answer) {
  * 0 : Good to continue.
  */
 function step() {
-    round = ((round + 1) % rounds_per_task);
-    total_count += 1;
+    round += 1;
 
-    if (total_count >= (num_systems * num_tasks * rounds_per_task)) {
-        return -1;
-    }
+    if (round != 0) {
+        // Not coming out of a practice round
 
-    if (round == 0) {
-        task_type = ((task_type + 1) % num_tasks);
-        if (task_type == 0) {
-            system_type = ((system_type + 1) % num_systems);
+        total_count += 1;
+        if (total_count >= (num_systems * num_tasks * rounds_per_task)) {
+            return -1;
+        }
+
+        if (round == rounds_per_task) {
+            // Going in to practice round
+            round = -1;
+
+            task_type = ((task_type + 1) % num_tasks);
+            if (task_type == 0) {
+                system_type = ((system_type + 1) % num_systems);
+            }
         }
     }
 
